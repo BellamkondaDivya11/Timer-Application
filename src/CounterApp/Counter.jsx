dd780e6c861e526660bb5ce90289aa7ef8e36919
Your Name <you@example.com>
2024-01-18 21:13:17 +0530

    first commit

---
+++ src/CounterApp/Counter.jsx
@@ -0,0 +1,23 @@
+import "./Counter.css";
+import { useState } from "react";
+function Counter() {
+  let [state, setState] = useState(0);
+  let [name, setName] = useState("Folks");
+
+  function increaseCount() {
+    setState(state + 1);
+    setName("Divya");
+    console.log(state);
+  }
+  return (
+    <div className="counter">
+      <h2>
+        {name} is increasing the Count Value {state}
+      </h2>
+
+      <button onClick={increaseCount}>Increase Count</button>
+    </div>
+  );
+}
+
+export default Counter;
