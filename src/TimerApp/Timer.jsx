7ff33b6eaab61d8d1494091dfe6589b85c415c1e
Your Name <you@example.com>
2024-01-20 21:03:23 +0530

    commited

--- src/TimerApp/Timer.jsx
+++ src/TimerApp/Timer.jsx
@@ -1,23 +1,44 @@
-import "./Counter.css";
-import { useState } from "react";
-function Counter() {
-  let [state, setState] = useState(0);
-  let [name, setName] = useState("Folks");
+import { useState, useRef } from "react";
 
-  function increaseCount() {
-    setState(state + 1);
-    setName("Divya");
-    console.log(state);
-  }
+function Timer(props) {
+  const [timer, setTimer] = useState(0);
+  const [timerid, setTimerid] = useState(0);
+  let ref1 = useRef(" Divya"); 
+  let h2Ref = useRef(null);
+  let startBtnRef = useRef(null);
+
+  const startTimer = () => {
+    h2Ref.current.style.color = "green";
+    startBtnRef.current.disabled = true;
+    ref1.current = setInterval(() => {
+      setTimer((prevstate) => prevstate + 1);
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    h2Ref.current.style.color = "red";
+    startBtnRef.current.disabled = false;
+    clearInterval(ref1.current); 
+  };
   return (
-    <div className="counter">
-      <h2>
-        {name} is increasing the Count Value {state}
-      </h2>
-
-      <button onClick={increaseCount}>Increase Count</button>
+    <div
+      style={{
+        width: "500px",
+        margin: "100px auto",
+        boxShadow: "0 0 10px black",
+        textAlign: "center",
+        padding: "50px",
+      }}
+    >
+      <h2 ref={h2Ref}>Timer : {timer}</h2>
+      <button ref={startBtnRef} onClick={startTimer}>
+        Start
+      </button>
+      &nbsp;&nbsp;&nbsp;
+      <button onClick={stopTimer}>Stop</button>
     </div>
   );
 }
 
-export default Counter;
+export default Timer;
+
